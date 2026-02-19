f16b3ac17a9c16134ecff97db0f49294345c51f8
Joel Neto <joel.antoniete@gmail.com>
2026-02-19 16:09:45 -0300

    finish service

---
+++ src/main/java/com/example/EncurtadorUrl/service/UrlService.java
@@ -0,0 +1,59 @@
+package com.example.EncurtadorUrl.service;
+
+import com.example.EncurtadorUrl.model.Url;
+import com.example.EncurtadorUrl.repository.UrlRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.Random;
+
+@Service
+public class UrlService {
+
+    @Autowired
+    private UrlRepository urlRepository;
+
+
+    public String shortenUrl(String originalUrl){
+        String shortUrl = generateShortUrl();
+
+        Url url = new Url();
+        url.setOriginalUrl(originalUrl);
+        url.setShortUrl(shortUrl);
+        url.setExpiration(LocalDateTime.now().plusDays(15));
+        urlRepository.save(url);
+
+        return shortUrl;
+    }
+
+    public Optional<Url> getOriginalUrl(String shortUrl) {
+        Optional<Url> urlOptional = urlRepository.findByUrl(shortUrl);
+
+        if (urlOptional.isPresent()) {
+            Url url = urlOptional.get();
+
+            if (url.getExpiration().isAfter(LocalDateTime.now())) {
+                return Optional.of(url);
+            } else {
+                urlRepository.delete(url);
+            }
+        }
+
+        return Optional.empty();
+    }
+
+
+    private String generateShortUrl(){
+        String characteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+        StringBuilder shortUrl = new StringBuilder();
+        Random random = new Random();
+        int length = 5 + random.nextInt(6);
+        for (int i = 0; i < length; i++) {
+            shortUrl.append(characteres.charAt(random.nextInt(characteres.length())));
+        }
+        return shortUrl.toString();
+    }
+}
